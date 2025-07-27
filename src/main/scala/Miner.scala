7ae65027a13247774746d447069b966eedb396ca
kenneth <kenneth12345645@gmail.com>
2025-07-27 21:13:31 +0800

    Complete Miner mining block messages

--- src/main/scala/Miner.scala
+++ src/main/scala/Miner.scala
@@ -1,23 +1,75 @@
-import Broker.MineCurrentBlockBrokerEvent
-import Miner.{MineCurrentBlockMinerEvent, Ready, ValidateBlock}
-import akka.actor.{Actor, ActorRef, Props}
+import BlockChain.{AddBlockEvent, GetLastHashEvent, getLastIndexEvent}
+import Broker.GetTransactionEvent
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.util.Timeout
 
+import scala.concurrent.duration.DurationInt
+import scala.util.{Failure, Success}
+
+//Miner is in charge of validating blocks, and mining the current block
 object Miner {
     sealed trait MinerEvent
-    case class ValidateBlock(block: Block) extends MinerEvent
+    case class ValidateBlock(block: Block, replyTo: ActorRef[Boolean]) extends MinerEvent
     case object MineCurrentBlockMinerEvent extends MinerEvent
-    case object Ready extends MinerEvent
 
-    def props(brokerActor: ActorRef) : Props = Props(new Miner(brokerActor))
-}
+    sealed trait MiningWorkflowEvent extends MinerEvent
+    case class obtainLastHashEvent(transactions: List[Transaction]) extends MiningWorkflowEvent
+    case class obtainLastIndexEvent(transactions: List[Transaction], prevHash: String) extends MiningWorkflowEvent
+    case class mineBlock(transactions: List[Transaction], prevHash: String, index: Int) extends MiningWorkflowEvent
 
-class Miner(brokerActor:ActorRef) extends Actor {
+    case class MiningFailed(error: String) extends MinerEvent
 
-    override def receive: Receive = {
-        case ValidateBlock(block) =>
-            val isValid = Block.isValidProof(block)
-            sender() ! isValid
-        case MineCurrentBlockMinerEvent => brokerActor ! MineCurrentBlockBrokerEvent
-        case Ready => ??? // TODO: To be implemented in case the miner is not ready
+    def apply(brokerActor: ActorRef[Broker.BrokerEvent], blockchainActor: ActorRef[BlockChain.BlockChainEvent]): Behavior[MinerEvent] =
+        minerBehavior(brokerActor, blockchainActor)
+
+    private def minerBehavior(
+                                 brokerActor: ActorRef[Broker.BrokerEvent],
+                                 blockchainActor: ActorRef[BlockChain.BlockChainEvent]
+                              ): Behavior[MinerEvent] = {
+        Behaviors.setup { context =>
+            implicit val timeout:Timeout = 3.seconds
+            Behaviors.receive { (context, message) =>
+                message match {
+                    case ValidateBlock(block, replyTo) =>
+                        val isValid = Block.isValidProof(block)
+                        replyTo ! isValid
+                        Behaviors.same
+                    case MineCurrentBlockMinerEvent =>
+                        context.log.info("Received mine block event")
+                        context.ask(brokerActor, ref => GetTransactionEvent(ref)) {
+                            case Success(transactions) => obtainLastHashEvent(transactions)
+                            case Failure(ex) => MiningFailed(ex.getMessage)
+                        }
+                        Behaviors.same
+                    case obtainLastHashEvent(transactions) =>
+                        context.log.info(s"Mining block with ${transactions.size} transactions")
+                        context.ask(blockchainActor, ref => GetLastHashEvent(ref)) {
+                            case Success(prevHash) => obtainLastIndexEvent(transactions, prevHash)
+                            case Failure(ex) => MiningFailed(ex.getMessage)
+                        }
+                        Behaviors.same
+                    case obtainLastIndexEvent(transactions, prevHash) =>
+                        context.log.info(s"Mining block with ${transactions.size} transactions and prevHash ${prevHash}")
+                        context.ask(blockchainActor, ref => getLastIndexEvent(ref)) {
+                        case Success(lastIndex) => mineBlock(transactions, prevHash, lastIndex)
+                        case Failure(ex) => MiningFailed(ex.getMessage)
+                    }
+                        Behaviors.same
+                    case mineBlock(transactions, prevHash, index) =>
+                        context.log.info(s"Mining block with ${transactions.size} transactions and prevHash ${prevHash}" +
+                            s"with index ${index}")
+                        val (hash, proof, timestamp) = Block.mineBlock(index, prevHash, transactions)
+                        val newBlock = Block(index, hash, prevHash, proof, timestamp, transactions)
+                        blockchainActor ! AddBlockEvent(newBlock)
+                        Behaviors.same
+                    case MiningFailed(error) =>
+                        context.log.error(error)
+                        Behaviors.same
+                }
+            }
+        }
     }
 }
+//In context.ask(...), the ref is an auto-generated temporary ActorRef[ReplyType] that you use to receive the response
+// from another actor.
